6acaf1faec12f1af75161cfeda5aaa2abaac2008
Alicia <52325101+AliciaC07@users.noreply.github.com>
2022-11-05 19:01:47 -0400

    Creacion de modelos

---
+++ app/src/main/java/com/aip/commerce_e/models/Product.java
@@ -0,0 +1,41 @@
+package com.aip.commerce_e.models;
+
+public class Product  {
+
+    private Integer id;
+    private String name;
+    private Float price;
+    private Category category;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
