7dcabed3e3c81a054c4d803077574b92f8508454
Dreamy <rubenosmani.dc@outlook.com>
2022-11-10 18:56:06 -0400

    Views

--- app/src/main/java/com/aip/commerce_e/LoginActivity.java
+++ app/src/main/java/com/aip/commerce_e/LoginActivity.java
@@ -1,5 +1,7 @@
 package com.aip.commerce_e;
 
+import android.content.Intent;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +12,12 @@ public class LoginActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginBtn = findViewById(R.id.login);
+        loginBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
 
     }
 }
