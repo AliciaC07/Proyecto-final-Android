05a61d1eea1d08fe9fdc48b4a2f4a12e5edd596a
Dreamy <rubenosmani.dc@outlook.com>
2022-11-17 00:15:51 -0400

    testing

--- app/src/main/java/com/aip/commerce_e/drawerFragments/category/CategoryFragment.java
+++ app/src/main/java/com/aip/commerce_e/drawerFragments/category/CategoryFragment.java
@@ -1,5 +1,7 @@
 package com.aip.commerce_e.drawerFragments.category;
 
+import android.annotation.SuppressLint;
+import android.widget.ImageView;
 import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
@@ -10,26 +12,41 @@ import android.view.View;
 import android.view.ViewGroup;
 import com.aip.commerce_e.R;
 import com.aip.commerce_e.RecyclerViewInterface;
+import com.aip.commerce_e.databinding.FragmentCategoryBinding;
 import com.aip.commerce_e.models.CategoryViewModel;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 public class CategoryFragment extends Fragment implements RecyclerViewInterface {
 
     private CategoryViewModel mViewModel;
+    private FragmentCategoryBinding binding;
 
     public static CategoryFragment newInstance() {
         return new CategoryFragment();
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        binding = FragmentCategoryBinding.inflate(inflater,container,false);
+
+        return binding.getRoot();
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(CategoryViewModel.class);
+        /*if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+            /*NavigationView navigationView = getActivity().findViewById(R.id.drawer_nav);
+            View headerView = navigationView.getHeaderView(0);
+            ImageView userImg = headerView.requireViewById(R.id.userImg);
+            userImg.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_shopping_basket_24));
+        }*/
         // TODO: Use the ViewModel
     }
 
