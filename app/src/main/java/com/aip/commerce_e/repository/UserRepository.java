3760b1bf00d26053a5a601d783be12455e03ac8f
Alicia <52325101+AliciaC07@users.noreply.github.com>
2022-11-17 00:15:55 -0400

    Navbar with user info.

--- app/src/main/java/com/aip/commerce_e/repository/UserRepository.java
+++ app/src/main/java/com/aip/commerce_e/repository/UserRepository.java
@@ -1,25 +1,27 @@
 package com.aip.commerce_e.repository;
 
 import android.app.Application;
+import android.os.AsyncTask;
 import androidx.lifecycle.LiveData;
 import com.aip.commerce_e.database.AppDatabase;
-import com.aip.commerce_e.database.ProductDao;
 import com.aip.commerce_e.database.UserDao;
-import com.aip.commerce_e.models.Product;
 import com.aip.commerce_e.models.User;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class UserRepository {
     private UserDao userDao;
     private LiveData<List<User>> allUsers;
     private LiveData<List<User>> activeAllUsers;
 
+
     public UserRepository(Application application) {
         AppDatabase db = AppDatabase.getInstance(application);
         userDao = db.userDao();
         allUsers = userDao.getAllUsers();
         activeAllUsers = userDao.getAllUserActive();
+
     }
 
     public void insert(User user) {
@@ -29,6 +31,9 @@ public class UserRepository {
     public void update(User user) {
         AppDatabase.databaseWriteExecutor.execute(() ->
                 userDao.update(user));
+    }
+    public User findUserByEmail(String email) throws ExecutionException, InterruptedException {
+       return new findUserAsyncTask(userDao).execute(email).get();
     }
 
     public void deleteAll() {
@@ -48,4 +53,16 @@ public class UserRepository {
         AppDatabase.databaseWriteExecutor.execute(() ->
                 userDao.deleteById(false, id));
     }
+    private static class findUserAsyncTask extends AsyncTask<String, Void, User>{
+        private UserDao asyncProdDao;
+
+        findUserAsyncTask(UserDao asyncProdDao){ this.asyncProdDao = asyncProdDao; }
+        @Override
+        protected User doInBackground(String... emails) {
+            User user = asyncProdDao.findByEmail(emails[0]);
+            return user;
+        }
+    }
+
+
 }
