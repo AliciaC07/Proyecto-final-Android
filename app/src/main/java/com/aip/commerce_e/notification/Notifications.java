b15be0e37c6d41cd2bd4b011d7d24dda7eb9a9e2
Alicia <52325101+AliciaC07@users.noreply.github.com>
2022-12-02 13:28:29 -0400

    Notification class for view.

---
+++ app/src/main/java/com/aip/commerce_e/notification/Notifications.java
@@ -0,0 +1,14 @@
+package com.aip.commerce_e.notification;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Notifications {
+
+    private String time;
+    private String title;
+    private String content;
+
+}
